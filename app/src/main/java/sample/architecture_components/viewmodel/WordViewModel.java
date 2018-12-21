c78423bfa6e3c7844d5e475ec686a7c0f54126a1
omaksymov <oleksandr.maksymov@gmail.com>
2018-12-21 19:06:44 +0200

    Implemented ViewModel, which is communicator between UI and repository, surviving data on activity rotations and other UI destroy actions

---
+++ app/src/main/java/sample/architecture_components/viewmodel/WordViewModel.java
@@ -0,0 +1,31 @@
+package sample.architecture_components.viewmodel;
+
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
+import android.arch.lifecycle.LiveData;
+import android.support.annotation.NonNull;
+
+import java.util.List;
+
+import sample.architecture_components.entities.Word;
+import sample.architecture_components.repository.WordRepository;
+
+public class WordViewModel extends AndroidViewModel {
+
+    private final WordRepository repository;
+    private final LiveData<List<Word>> allWords;
+
+    public WordViewModel(@NonNull Application application) {
+        super(application);
+        repository = new WordRepository(application);
+        allWords = repository.getAllWords();
+    }
+
+    public LiveData<List<Word>> getAllWords() {
+        return allWords;
+    }
+
+    public void insert(Word word) {
+        repository.insert(word);
+    }
+}
