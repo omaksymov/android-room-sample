98e5452d0204d7271b4180a25a2091e212ef0b1c
omaksymov <oleksandr.maksymov@gmail.com>
2018-12-21 18:11:36 +0200

    Created Word Room Entity

---
+++ app/src/main/java/sample/architecture_components/entities/Word.java
@@ -0,0 +1,32 @@
+package sample.architecture_components.entities;
+
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+import android.support.annotation.NonNull;
+
+@Entity(tableName = "word_table")
+public class Word {
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+
+    @NonNull
+    @ColumnInfo(name = "word")
+    private String word;
+
+    public Word(@NonNull String word) {
+        this.word = word;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
